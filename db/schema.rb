079e90ffbec1352890d6cf6ba50f98acd9433cd6
askrynnikov <5101233@gmail.com>
2016-12-21 21:13:15 +0300

    users initial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221161523) do
+ActiveRecord::Schema.define(version: 20161221180000) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20161221161523) do
     t.datetime "datetime"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
