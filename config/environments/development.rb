3ad27408dacc964e056b82c849310c99be520cab
askrynnikov <5101233@gmail.com>
2017-01-04 19:24:21 +0300

    Fix error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,4 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
-
-  config.serve_static_assets = true
 end
