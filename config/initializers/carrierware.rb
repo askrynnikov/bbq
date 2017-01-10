015a4cf74982a6d8a9adab3a92a6afc3dd3adf00
askrynnikov <5101233@gmail.com>
2017-01-10 23:47:45 +0300

    Experiment

--- config/initializers/carrierware.rb
+++ config/initializers/carrierware.rb
@@ -4,7 +4,7 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
+    # config.fog_provider = 'fog/aws'
 
     config.fog_credentials = {
       provider: 'AWS',
