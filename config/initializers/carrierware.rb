eabf781e2a296f04c400fe0ca4f7a9744b520394
askrynnikov <5101233@gmail.com>
2017-01-10 23:03:22 +0300

    Fix bug

--- config/initializers/carrierware.rb
+++ config/initializers/carrierware.rb
@@ -4,6 +4,8 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id:     ENV['S3_ACCESS_key'],
