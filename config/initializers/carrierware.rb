cccecabf5741c88af53164a0e80f51f545f89956
askrynnikov <5101233@gmail.com>
2017-01-10 23:24:14 +0300

    Fix bug

--- config/initializers/carrierware.rb
+++ config/initializers/carrierware.rb
@@ -8,6 +8,7 @@ if Rails.env.production?
 
     config.fog_credentials = {
       provider: 'AWS',
+      region:                'Oregon',
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
